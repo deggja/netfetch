085ad088a05a8ae2da8ad32319acf658d6ffbbe3
deggja <danieldagfinrud@gmail.com>
2023-11-22 18:21:11 +0100

    Update: changed logic to look for network policies instead of network traffic as it has too many dependencies for now
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- pkg/k8s/scanner.go
+++ pkg/k8s/scanner.go
@@ -3,20 +3,24 @@ package k8s
 import (
 	"context"
 	"fmt"
-	"os"
-	"os/exec"
 	"path/filepath"
-	"time"
 
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/util/homedir"
 )
 
-// Scan namespace for pods function
-func ScanNamespace(namespace string) {
-	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
+// ScanNetworkPolicies scans all non-system namespaces for network policies
+func ScanNetworkPolicies() {
+	// Use the default kubeconfig path if running outside the cluster
+	var kubeconfig string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = filepath.Join(home, ".kube", "config")
+	}
+
+	// Use the current context in kubeconfig
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
 		fmt.Printf("Error building kubeconfig: %s\n", err)
@@ -29,64 +33,50 @@ func ScanNamespace(namespace string) {
 		return
 	}
 
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		fmt.Printf("Error listing pods: %s\n", err)
+		fmt.Printf("Error listing namespaces: %s\n", err)
 		return
 	}
 
-	for _, pod := range pods.Items {
-		fmt.Printf("Name: %s, Namespace: %s\n", pod.Name, pod.Namespace)
-	}
-}
-
-// ListPods lists all pods in the specified namespace and returns their names
-func ListPods(namespace string) ([]string, error) {
-	kubeconfig := filepath.Join(homedir.HomeDir(), ".kube", "config")
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
-	if err != nil {
-		return nil, fmt.Errorf("Error building kubeconfig: %s", err)
-	}
-
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		return nil, fmt.Errorf("Error creating Kubernetes client: %s", err)
-	}
-
-	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		return nil, fmt.Errorf("Error listing pods: %s", err)
-	}
-
-	var podNames []string
-	for _, pod := range pods.Items {
-		podNames = append(podNames, pod.Name)
-	}
-
-	return podNames, nil
-}
-
-// SniffPods sniffs network traffic from a list of pods in a namespace
-func SniffPods(podNames []string, namespace string) error {
-	for _, podName := range podNames {
-		fmt.Printf("Sniffing pod: %s\n", podName)
-		err := SniffPodTraffic(podName, namespace)
-		if err != nil {
-			fmt.Printf("Error sniffing pod %s: %s\n", podName, err)
-			// Decide whether to continue, return, or handle the error differently
+	for _, ns := range namespaces.Items {
+		if isSystemNamespace(ns.Name) {
+			continue
 		}
-		// Add a delay between sniffs to manage resource usage, if necessary
-		time.Sleep(5 * time.Second)
+
+		policies, err := clientset.NetworkingV1().NetworkPolicies(ns.Name).List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			fmt.Printf("Error listing network policies in namespace %s: %s\n", ns.Name, err)
+			continue
+		}
+
+		if !hasDefaultDenyAllPolicy(policies.Items) {
+			fmt.Printf("The namespace %s does not contain a default deny all network policy.\n", ns.Name)
+		}
 	}
-	return nil
 }
 
-// SniffPodTraffic runs ksniff on a specific pod
-func SniffPodTraffic(podName, namespace string) error {
-	cmd := exec.Command("kubectl", "sniff", podName, "-n", namespace, "-o", podName+".pcap")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+// hasDefaultDenyAllPolicy checks if the list of policies includes a default deny all policy
+func hasDefaultDenyAllPolicy(policies []networkingv1.NetworkPolicy) bool {
+	for _, policy := range policies {
+		if isDefaultDenyAllPolicy(policy) {
+			return true
+		}
+	}
+	return false
+}
 
-	fmt.Printf("Starting network sniffing on pod %s in namespace %s\n", podName, namespace)
-	return cmd.Run()
+// isDefaultDenyAllPolicy checks if a single network policy is a default deny all policy
+func isDefaultDenyAllPolicy(policy networkingv1.NetworkPolicy) bool {
+	return len(policy.Spec.Ingress) == 0 && len(policy.Spec.Egress) == 0
+}
+
+// isSystemNamespace checks if the given namespace is a system namespace
+func isSystemNamespace(namespace string) bool {
+	switch namespace {
+	case "kube-system", "tigera-operator", "kube-public", "kube-node-lease":
+		return true
+	default:
+		return false
+	}
 }
