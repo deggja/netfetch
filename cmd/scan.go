d2ef17f9f5b0e3d0a29f4d29a1e7c80c925ce4cc
deggja <danieldagfinrud@gmail.com>
2023-11-22 18:21:11 +0100

    Update: changed logic to look for network policies instead of network traffic as it has too many dependencies for now
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,50 +1,19 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/deggja/netfetch/pkg/k8s"
-	"github.com/deggja/netfetch/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
-var namespace string
-
 var scanCmd = &cobra.Command{
 	Use:   "scan",
-	Short: "Scan a Kubernetes namespace",
-	Long:  `Scan a specified Kubernetes namespace for pod-to-pod network traffic.`,
+	Short: "Scan Kubernetes namespaces for network policies",
+	Long:  `Scan all non-system Kubernetes namespaces for network policies and compare them with predefined standards.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if namespace == "" {
-			fmt.Println("Please specify a namespace")
-			return
-		}
-		k8s.ScanNamespace(namespace)
-
-		podNames, err := k8s.ListPods(namespace)
-		if err != nil {
-			fmt.Printf("Error listing pods: %s\n", err)
-			return
-		}
-
-		err = k8s.SniffPods(podNames, namespace)
-		if err != nil {
-			fmt.Printf("Error sniffing pods: %s\n", err)
-			return
-		}
-
-		if !utils.CheckDependency("kubectl") {
-			fmt.Println("kubectl is not installed. ", utils.InstallInstructions("kubectl"))
-			return
-		}
-		if !utils.CheckDependency("ksniff") {
-			fmt.Println("ksniff is not installed. ", utils.InstallInstructions("ksniff"))
-			return
-		}
+		k8s.ScanNetworkPolicies()
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(scanCmd)
-	scanCmd.Flags().StringVarP(&namespace, "namespace", "n", "", "Specify the namespace to scan")
 }
