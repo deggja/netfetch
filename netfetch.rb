c8fa76a57ce46e04e39c190cede269e457cfea50
deggja <danieldagfinrud@gmail.com>
2023-11-23 22:33:21 +0100

    Feat: added logic to detect other network policies in non system namespaces + print the ones that are not covered by any policy and prompt for creation
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- netfetch.rb
+++ netfetch.rb
@@ -1,7 +1,7 @@
 class Netfetch < Formula
     desc "CLI tool to scan for network policies in Kubernetes clusters"
     homepage "https://github.com/deggja/netfetch"
-    url "https://github.com/deggja/netfetch/releases/download/0.0.5/netfetch_0.0.5_darwin_amd64.tar.gz"
+    url "https://github.com/deggja/netfetch/releases/download/0.0.6/netfetch_0.0.6_darwin_amd64.tar.gz"
     sha256 "0d9ba6bb2c0509b8a1d97bc680fccf48f2689e4f4195865aeb4dfa08356d8db0"
   
     def install
