040f1c2187618ac735164e484125e336b9f10e17
deggja <danieldagfinrud@gmail.com>
2023-11-24 12:31:15 +0100

    Update: added new file path to netfetch.rb as well
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- netfetch.rb
+++ netfetch.rb
@@ -1,7 +1,7 @@
 class Netfetch < Formula
     desc "CLI tool to scan for network policies in Kubernetes clusters"
     homepage "https://github.com/deggja/netfetch"
-    url "https://github.com/deggja/netfetch/releases/download/0.0.9/netfetch_0.0.9_darwin_amd64.tar.gz"
+    url "https://github.com/deggja/netfetch/releases/download/0.0.15/netfetch_0.0.15_darwin_amd64.tar.gz"
     sha256 "2bbf7cb3d50e477d45d6557417048a07b1d1f3884543cad3583049ef04072857"
   
     def install
@@ -12,4 +12,3 @@ class Netfetch < Formula
       system "#{bin}/netfetch", "--version"
     end
   end
-  
