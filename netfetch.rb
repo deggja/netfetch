2e0d4a1d7b177f7a434d66e2e68bf58833966d6e
deggja <danieldagfinrud@gmail.com>
2023-11-24 10:17:39 +0100

    Feat: added functionality to add output to a file after successfull scan
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- netfetch.rb
+++ netfetch.rb
@@ -1,7 +1,7 @@
 class Netfetch < Formula
     desc "CLI tool to scan for network policies in Kubernetes clusters"
     homepage "https://github.com/deggja/netfetch"
-    url "https://github.com/deggja/netfetch/releases/download/0.0.8/netfetch_0.0.8_darwin_amd64.tar.gz"
+    url "https://github.com/deggja/netfetch/releases/download/0.0.9/netfetch_0.0.9_darwin_amd64.tar.gz"
     sha256 "0d9ba6bb2c0509b8a1d97bc680fccf48f2689e4f4195865aeb4dfa08356d8db0"
   
     def install
