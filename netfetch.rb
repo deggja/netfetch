64b8c94e483f2fb8baa8dc6ee5738b0e03489bdd
deggja <danieldagfinrud@gmail.com>
2023-11-26 16:57:15 +0100

    Updaet: new homebrew version
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- netfetch.rb
+++ netfetch.rb
@@ -1,8 +1,8 @@
 class Netfetch < Formula
     desc "CLI tool to scan for network policies in Kubernetes clusters and provide a score"
     homepage "https://github.com/deggja/netfetch"
-    url "https://github.com/deggja/netfetch/releases/download/0.0.35/netfetch_0.0.35_darwin_amd64.tar.gz"
-    sha256 "dfbbf121e60641ac5b1bad7abf297510178765e7b2f87049040fe5da076f7143"
+    url "https://github.com/deggja/netfetch/releases/download/0.0.37/netfetch_0.0.37_darwin_amd64.tar.gz"
+    sha256 "ec6fbf079f8c313b0bbe562c68f9213d2f6a0d4630f3c8aa65dca852f92518c8"
   
     def install
       bin.install "netfetch"
