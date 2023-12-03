e25253362561c2bbb47eb6c59dd57959fb209dad
deggja <danieldagfinrud@gmail.com>
2023-12-03 03:08:02 +0100

    Update: sectionized the cluster map a tiny bit more, wip
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- frontend/dash/src/Viz.vue
+++ frontend/dash/src/Viz.vue
@@ -7,35 +7,39 @@
 
   // Define the custom cluster force
   function forceCluster(centers) {
-  let nodes;
-  let strength = 0.1; // Default strength value
+    let nodes;
+    let strength = 0.1;
 
-  function force(alpha) {
-    const l = alpha * strength;
-    nodes.forEach(node => {
-      const center = centers[node.cluster];
-      if (center) {
-        node.vx -= (node.x - center.x) * l;
-        node.vy -= (node.y - center.y) * l;
-      }
-    });
-  }
-
-  force.initialize = function(_) {
-    nodes = _;
-  };
-
-  force.strength = function(_) {
-    // This allows us to chain strength like other d3 forces if we want to set a strength
-    if (typeof _ === 'number') {
-      strength = _;
-      return force;
+    function force(alpha) {
+      const l = alpha * strength;
+      nodes.forEach(node => {
+        const center = centers[node.cluster];
+        if (center) {
+          node.vx -= (node.x - center.x) * l;
+          node.vy -= (node.y - center.y) * l;
+        }
+      });
     }
-    return strength;
-  };
 
-  return force;
+    force.initialize = function(_) {
+      nodes = _;
+    };
+
+    force.strength = function(_) {
+      if (typeof _ === 'number') {
+        strength = _;
+      } else {
+        const clusterNodeCount = {};
+        nodes.forEach(node => {
+          strength = Math.min(1, 10 / (clusterNodeCount[node.cluster] || 1));
+        });
+      }
+      return force;
+    };
+
+    return force;
   }
+
 
   export default {
   name: 'NetworkPolicyVisualization',
@@ -69,25 +73,37 @@
       return;
     }
 
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
     const width = this.$refs.vizContainer.clientWidth;
     const height = this.$refs.vizContainer.clientHeight;
     const nodes = [];
     const links = [];
-    const clusterMap = new Map();
+    const namespaceClusterMap = new Map();
 
-    data.forEach((policy, index) => {
-      const policyNode = { id: policy.name, type: 'policy', cluster: index };
+    // Group policies by namespace and prepare nodes and links
+    let clusterIndex = 0;
+    data.forEach(policy => {
+      if (!namespaceClusterMap.has(policy.namespace)) {
+        namespaceClusterMap.set(policy.namespace, clusterIndex++);
+      }
+      const namespaceCluster = namespaceClusterMap.get(policy.namespace);
+
+      const policyNode = { id: policy.name, type: 'policy', cluster: namespaceCluster };
       nodes.push(policyNode);
-      clusterMap.set(policy.name, index);
 
       policy.targetPods.forEach(podName => {
-        const podNode = { id: podName, type: 'pod', cluster: index };
+        const podNode = { id: podName, type: 'pod', cluster: namespaceCluster };
         if (!nodes.some(n => n.id === podName)) {
           nodes.push(podNode);
         }
         links.push({ source: policy.name, target: podName });
       });
     });
+
+    const clusterNodeCounts = nodes.reduce((counts, node) => {
+      counts[node.cluster] = (counts[node.cluster] || 0) + 1;
+      return counts;
+    }, {});
 
     // Tooltip for full text display
     const tooltip = d3.select('body').append('div')
@@ -100,17 +116,20 @@
           .style('pointer-events', 'none');
 
     // Calculate cluster centers
-    const uniqueClusters = Array.from(new Set(data.map((_, index) => index)));
-    const numberOfClusters = uniqueClusters.length;
-    const gridSize = Math.ceil(Math.sqrt(numberOfClusters));
-    const sectionWidth = width / gridSize;
-    const sectionHeight = height / gridSize;
-    const clusterCenters = {};
+    const gridColumns = Math.ceil(Math.sqrt(namespaceClusterMap.size));
+    const gridRows = Math.ceil(namespaceClusterMap.size / gridColumns);
+    const sectionWidth = width / gridColumns;
+    const sectionHeight = height / gridRows;
+    const boundaryPadding = Math.min(sectionWidth, sectionHeight) / 4;
 
-    uniqueClusters.forEach((cluster, index) => {
-      const x = (index % gridSize) * sectionWidth + sectionWidth / 2;
-      const y = Math.floor(index / gridSize) * sectionHeight + sectionHeight / 2;
-      clusterCenters[cluster] = { x, y };
+    const clusterCenters = {};
+    const namespaces = Array.from(namespaceClusterMap.keys());
+    namespaces.forEach((namespace, index) => {
+      const columnIndex = index % gridColumns;
+      const rowIndex = Math.floor(index / gridColumns);
+      const x = columnIndex * sectionWidth + sectionWidth / 2;
+      const y = rowIndex * sectionHeight + sectionHeight / 2;
+      clusterCenters[namespaceClusterMap.get(namespace)] = { x, y };
     });
 
     // Assign initial positions
@@ -134,19 +153,58 @@
 
     const containerGroup = svg.append('g');
 
+    function drawClusterBoundaries() {
+      containerGroup.selectAll('.cluster-boundary')
+        .data(namespaces)
+        .enter().append('rect')
+        .attr('class', 'cluster-boundary')
+        .attr('x', d => clusterCenters[namespaceClusterMap.get(d)].x - boundaryPadding / 2)
+        .attr('y', d => clusterCenters[namespaceClusterMap.get(d)].y - boundaryPadding / 2)
+        .attr('width', boundaryPadding)
+        .attr('height', boundaryPadding)
+        .attr('fill', 'none')
+        .attr('stroke', '#ccc')
+        .attr('stroke-dasharray', '4');
+      }
+
+    drawClusterBoundaries();
+
+    createNamespaceLabels(containerGroup, namespaces, clusterCenters, namespaceClusterMap);
+
     // Console log for debugging
     console.log("Nodes before simulation:", nodes);
     console.log("Links:", links);
     console.log("Cluster Centers:", clusterCenters);
 
+    const nodeRadius = 10;
+    const collisionRadius = nodeRadius * 1.2;
+
+    const linkDistance = (d) => {
+    const clusterSize = clusterNodeCounts[d.source.cluster] || 1;
+    const baseDistance = 50;
+    const additionalDistancePerNode = 10;
+    
+    return baseDistance + (clusterSize * additionalDistancePerNode);
+  };
+
     // Create the simulation with appropriate forces
     const simulation = d3.forceSimulation(nodes)
-      .force('link', d3.forceLink(links).id(d => d.id).distance(50))
+      .force('link', d3.forceLink(links).id(d => d.id).distance(linkDistance))
       .force('charge', d3.forceManyBody().strength(-50))
       .force('center', d3.forceCenter(width / 2, height / 2))
-      .force('cluster', forceCluster(clusterCenters).strength(0.5))
-      .force('x', d3.forceX().x(d => clusterCenters[d.cluster].x))
-      .force('y', d3.forceY().y(d => clusterCenters[d.cluster].y));
+      .force('collide', d3.forceCollide(collisionRadius).strength(1))
+      .force('cluster', forceCluster(clusterCenters).strength(0.8))
+      .force('x', d3.forceX().x(d => clusterCenters[d.cluster].x).strength(0.5))
+      .force('y', d3.forceY().y(d => clusterCenters[d.cluster].y).strength(0.5));
+
+    const node = containerGroup.append('g')
+      .selectAll('circle')
+      .data(nodes)
+      .join('circle')
+      .attr('r', 5)
+      .attr('fill', d => color(namespaceClusterMap.get(d.namespace)))
+      .call(drag(simulation));
+
 
     // Legends
     const legendGroup = svg.append('g')
@@ -194,16 +252,6 @@
           .join('line')
           .attr('stroke-width', d => Math.sqrt(d.value));
 
-    const node = containerGroup.append('g')
-        .attr('stroke', '#fff')
-        .attr('stroke-width', 1.5)
-        .selectAll('circle')
-        .data(nodes)
-        .join('circle')
-        .attr('r', 5)
-        .attr('fill', d => d.type === 'policy' ? '#007bff' : '#28a745')
-        .call(drag(simulation));
-
     // Add labels with hover functionality to show full text
     const labels = containerGroup.append('g')
         .attr('class', 'labels')
@@ -226,47 +274,38 @@
             tooltip.style('visibility', 'hidden');
         });
 
-    // Update positions on each tick
-    simulation.on('tick', () => {
-      nodes.forEach(node => {
-        if (isNaN(node.x) || isNaN(node.y)) {
-          console.error('NaN position for node:', node);
-        }
-      });
-
-      link
-        .attr('x1', d => d.source.x)
-        .attr('y1', d => d.source.y)
-        .attr('x2', d => d.target.x)
-        .attr('y2', d => d.target.y);
-
-      node
-        .attr('cx', d => d.x)
-        .attr('cy', d => d.y);
-
-      // Adjust label positions to prevent overlap
-      labels.attr('x', d => d.x)
-          .attr('y', d => d.y)
-          .each(function (d, i) {
-            for (let j = i + 1; j < nodes.length; ++j) {
-              const other = labels.nodes()[j];
-              const thisBox = this.getBBox();
-              const otherBox = other.getBBox();
-              
-              const deltaX = d.x - nodes[j].x;
-              const deltaY = d.y - nodes[j].y;
-              const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-              
-              const padding = 2;
-              if (dist < thisBox.width + otherBox.width + padding) {
-                d.x += deltaX / dist * padding;
-                d.y += deltaY / dist * padding;
-                nodes[j].x -= deltaX / dist * padding;
-                nodes[j].y -= deltaY / dist * padding;
-              }
+        simulation.on('tick', () => {
+          const padding = boundaryPadding / 2;
+          // Keep nodes within the boundaries
+          nodes.forEach(node => {
+            const center = clusterCenters[node.cluster];
+            if (!center) {
+              console.error('No center found for node', node);
+              return; // Skip this node to avoid errors
             }
+            // Ensure nodes are within the boundary minus the padding
+            node.x = Math.max(center.x - padding, Math.min(center.x + padding, node.x));
+            node.y = Math.max(center.y - padding, Math.min(center.y + padding, node.y));
+          });
+
+          // Update link positions, checking for NaN values
+          link
+            .attr('x1', d => isNaN(d.source.x) ? 0 : d.source.x)
+            .attr('y1', d => isNaN(d.source.y) ? 0 : d.source.y)
+            .attr('x2', d => isNaN(d.target.x) ? 0 : d.target.x)
+            .attr('y2', d => isNaN(d.target.y) ? 0 : d.target.y);
+
+          // Update node positions, checking for NaN values
+          node
+            .attr('cx', d => isNaN(d.x) ? 0 : d.x)
+            .attr('cy', d => isNaN(d.y) ? 0 : d.y);
+
+          // Update label positions, checking for NaN values
+          labels
+            .attr('x', d => isNaN(d.x) ? 0 : d.x)
+            .attr('y', d => isNaN(d.y) ? 0 : d.y);
         });
-    });
+
 
     // Drag functionality
     function drag(simulation) {
@@ -292,6 +331,22 @@
           .on('drag', dragged)
           .on('end', dragended);
           }
+
+      function createNamespaceLabels() {
+        containerGroup.selectAll('.namespace-label')
+          .data(namespaces)
+          .enter().append('text')
+          .attr('class', 'namespace-label')
+          .attr('x', d => clusterCenters[namespaceClusterMap.get(d)].x)
+          .attr('y', d => clusterCenters[namespaceClusterMap.get(d)].y - boundaryPadding / 2)
+          .attr('text-anchor', 'middle')
+          .attr('fill', '#555')
+          .text(d => d)
+          .attr('font-size', '12px')
+          .attr('font-weight', 'bold');
+        }
+
+        createNamespaceLabels();
       },
     },
     watch: {
@@ -319,11 +374,23 @@
   font-size: 10px;
   pointer-events: none;
 }
+
 .network-policy-visualization {
   width: 100%;
   padding: 20px;
   height: 600px;
 }
+
+.cluster-boundary {
+  stroke-opacity: 0.6;
+  stroke-width: 1px;
+}
+
+.namespace-label {
+  font-size: 12px;
+  font-weight: bold;
+}
+
 .tooltip {
     position: absolute;
     visibility: hidden;
