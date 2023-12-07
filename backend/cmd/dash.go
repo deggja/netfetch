b575b8fbff0e59c0a1dd01b925018063d0a1250e
deggja <danieldagfinrud@gmail.com>
2023-12-07 09:56:50 +0100

    feat: added network maps
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- backend/cmd/dash.go
+++ backend/cmd/dash.go
@@ -7,9 +7,9 @@ import (
 	"net/http"
 
 	_ "github.com/deggja/netfetch/backend/statik"
+	"github.com/rakyll/statik/fs"
 
 	"github.com/deggja/netfetch/backend/pkg/k8s"
-	"github.com/rakyll/statik/fs"
 	"github.com/rs/cors"
 	"github.com/spf13/cobra"
 )
@@ -54,32 +54,32 @@ func startDashboardServer() {
 	}
 }
 
-func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-	// Check if we are in development mode
-	isDevelopment := true // You can use an environment variable or a config flag to set this
-	if isDevelopment {
-		// Redirect to the Vue dev server
-		vueDevServer := "http://localhost:8081"
-		http.Redirect(w, r, vueDevServer+r.RequestURI, http.StatusTemporaryRedirect)
-	} else {
-		// Serve the embedded frontend using statik
-		statikFS, err := fs.New()
-		if err != nil {
-			log.Fatal(err)
-		}
-		http.FileServer(statikFS).ServeHTTP(w, r)
-	}
-}
-
 // func dashboardHandler(w http.ResponseWriter, r *http.Request) {
-// 	statikFS, err := fs.New()
-// 	if err != nil {
-// 		log.Fatal(err)
+// 	// Check if we are in development mode
+// 	isDevelopment := true // You can use an environment variable or a config flag to set this
+// 	if isDevelopment {
+// 		// Redirect to the Vue dev server
+// 		vueDevServer := "http://localhost:8081"
+// 		http.Redirect(w, r, vueDevServer+r.RequestURI, http.StatusTemporaryRedirect)
+// 	} else {
+// 		// Serve the embedded frontend using statik
+// 		statikFS, err := fs.New()
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		http.FileServer(statikFS).ServeHTTP(w, r)
 // 	}
-
-// 	// Serve the embedded frontend
-// 	http.FileServer(statikFS).ServeHTTP(w, r)
 // }
+
+func dashboardHandler(w http.ResponseWriter, r *http.Request) {
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Serve the embedded frontend
+	http.FileServer(statikFS).ServeHTTP(w, r)
+}
 
 // handleNamespacesWithPoliciesRequest handles the HTTP request for serving a list of namespaces with network policies.
 func handleNamespacesWithPoliciesRequest(w http.ResponseWriter, r *http.Request) {
