91549536b15c6d587ac32ffd5e3d6d4538b7914f
deggja <danieldagfinrud@gmail.com>
2023-12-08 19:13:30 +0100

    feat: update homebrew and versioning
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- backend/cmd/root.go
+++ backend/cmd/root.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const version = "0.0.50"
+const version = "0.0.53"
 
 var rootCmd = &cobra.Command{
 	Use:   "netfetch",
--- netfetch.rb
+++ netfetch.rb
@@ -1,8 +1,8 @@
 class Netfetch < Formula
-    desc "CLI tool to scan for network policies in Kubernetes clusters and provide a score"
+    desc "CLI tool to scan for network policies in Kubernetes clusters/namespaces and provide a score based on the amount of untargeted workloads"
     homepage "https://github.com/deggja/netfetch"
-    url "https://github.com/deggja/netfetch/releases/download/0.0.50/netfetch_0.0.50_darwin_amd64.tar.gz"
-    sha256 "99e6f149065d5589d5306266320dbefc130c0a61f5ae2e67514de54df14f0d91"
+    url "https://github.com/deggja/netfetch/releases/download/0.0.53/netfetch_0.0.53_darwin_amd64.tar.gz"
+    sha256 "4bc8092b5df77ce87c6dafa3d46e84779490985dbc2317072b00dd5bafeee61d"
   
     def install
       bin.install "netfetch"
