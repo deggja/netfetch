e7eb8c75e292a8f884898ab285bb133cc63e6349
deggja <deggja@users.noreply.github.com>
2023-12-12 09:21:27 +0000

    feat: update brew for version 1.1.0

--- netfetch.rb
+++ netfetch.rb
@@ -1,8 +1,8 @@
 class Netfetch < Formula
     desc "CLI tool to scan for network policies in Kubernetes clusters/namespaces and provide a score based on the amount of untargeted workloads"
     homepage "https://github.com/deggja/netfetch"
-    url "https://github.com/deggja/netfetch/releases/download/1.0.4/netfetch_1.0.4_darwin_amd64.tar.gz"
-    sha256 "c74157ff160837d0878db010f23aa5e506e4463308448e439023fad9991d2489"
+    url "https://github.com/deggja/netfetch/releases/download/1.1.0/netfetch_1.1.0_darwin_amd64.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   
     def install
       bin.install "netfetch"
