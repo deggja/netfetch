5be7c55bff41246dbf55efd2076c0b6cdd05466a
deggja <danieldagfinrud@gmail.com>
2024-02-09 23:40:54 +0100

    feat: update homebrew formula
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- netfetch.rb
+++ netfetch.rb
@@ -3,11 +3,11 @@ class Netfetch < Formula
   homepage "https://github.com/deggja/netfetch"
 
   if OS.mac?
-    url "https://github.com/deggja/netfetch/releases/download/3.2.8/netfetch_3.2.8_darwin_amd64.tar.gz"
-    sha256 "71b17743763290f800d44647e5ed981d1b11066320591bc6c4ed7053d985e9ea"
+    url "https://github.com/deggja/netfetch/releases/download/3.2.11/netfetch_3.2.11_darwin_amd64.tar.gz"
+    sha256 "2f9954637f21af44d7d7c1ed9b503195a38d19981b2ef8657ae01a6d7928d8d7"
   elsif OS.linux?
-    url "https://github.com/deggja/netfetch/releases/download/3.2.8/netfetch_3.2.8_linux_amd64.tar.gz"
-    sha256 "7461d611f0647cb7f756c93a9dc8f6ac32f9fc3b7c5875eee325601450ebb5c7"
+    url "https://github.com/deggja/netfetch/releases/download/3.2.11/netfetch_3.2.11_linux_amd64.tar.gz"
+    sha256 "985d48deaaccbcd26c28393734533097e0130e354fdb5a02529bde48e2787e49"
   end
 
   def install
