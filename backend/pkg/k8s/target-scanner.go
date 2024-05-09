8b5e00f0bc04e00e22a5bdc8fb8e6bf8d01986ff
deggja <danieldagfinrud@gmail.com>
2024-05-09 21:04:44 +0200

    feat: lists all pods targeted by specific policy scanned
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- backend/pkg/k8s/target-scanner.go
+++ backend/pkg/k8s/target-scanner.go
@@ -7,6 +7,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/kubernetes"
@@ -54,4 +55,38 @@ func GetAllNonSystemNamespaces(dynamicClient dynamic.Interface) ([]string, error
 		}
 	}
 	return namespaces, nil
+}
+
+// ListPodsTargetedByNetworkPolicy lists all pods targeted by the given network policy in the specified namespace.
+func ListPodsTargetedByNetworkPolicy(cynamicClient dynamic.Interface, policy *unstructured.Unstructured, namespace string) ([]string, error) {
+	// Retrieve the PodSelector (matchLabels)
+	podSelector, found, err := unstructured.NestedMap(policy.Object, "spec", "podSelector", "matchLabels")
+	if err != nil {
+		return nil, fmt.Errorf("failed to retrieve pod selector from network policy %s: %v", policy.GetName(), err)
+	}
+
+	// Check if the selector is empty
+	selector := make(labels.Set)
+	if found && len(podSelector) > 0 {
+		for key, value := range podSelector {
+			if strValue, ok := value.(string); ok {
+				selector[key] = strValue
+			} else {
+				return nil, fmt.Errorf("invalid type for selector value %v in policy %s", value, policy.GetName())
+			}
+		}
+	}
+
+	// Fetch pods based on the selector
+	pods, err := clientset.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{LabelSelector: selector.AsSelectorPreValidated().String()})
+	if err != nil {
+		return nil, fmt.Errorf("error listing pods in namespace %s: %v", namespace, err)
+	}
+
+	var targetedPods []string
+	for _, pod := range pods.Items {
+		targetedPods = append(targetedPods, pod.Name)
+	}
+
+	return targetedPods, nil
 }
