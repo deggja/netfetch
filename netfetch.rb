c9680586670f90ecf20e86ccba949215bab945b5
deggja <danieldagfinrud@gmail.com>
2024-05-12 22:58:07 +0200

    chore: update version in homebrew formulae
    Signed-off-by: deggja <danieldagfinrud@gmail.com>

--- netfetch.rb
+++ netfetch.rb
@@ -3,11 +3,11 @@ class Netfetch < Formula
   homepage "https://github.com/deggja/netfetch"
 
   if OS.mac?
-    url "https://github.com/deggja/netfetch/releases/download/5.0.1/netfetch_5.0.1_darwin_amd64.tar.gz"
-    sha256 "35674d84e214bd096339fec2a2c27499d073531a219ab7d3f56a6ef5b874ba8d"
+    url "https://github.com/deggja/netfetch/releases/download/5.2.2/netfetch_5.2.2_darwin_amd64.tar.gz"
+    sha256 "4a8687706fb9ca8df94f020c27540c3296c3032a5e0f6c0e1427b24438d8d3e7"
   elsif OS.linux?
-    url "https://github.com/deggja/netfetch/releases/download/5.0.1/netfetch_5.0.1_linux_amd64.tar.gz"
-    sha256 "442f571b92376429b56de827a6e00628afbd59a20f932fc6fe566a2440ed2877"
+    url "https://github.com/deggja/netfetch/releases/download/5.2.2/netfetch_5.2.2_linux_amd64.tar.gz"
+    sha256 "7f220340524cf8bb8822ac589bdd8ad83e5e27a65cf8083b06b4cce78220f212"
   end
 
   def install
